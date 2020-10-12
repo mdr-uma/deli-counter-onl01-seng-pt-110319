eff98c3b226b4e1311c4a41d778ec6e13e4ab669
Uma Manandhar <um_2003_8@hotmail.com>
2020-10-12 16:23:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,9 +8,9 @@ def line(customer)
     puts 'The line is currently empty.'
   else
     # binding.pry
-    current_line = 'The line is currently'
+    current_line = 'The line is currently: '
     customer.each.with_index(1) do |i, person|
-      puts ''
+      current_line << "#{i}". "#{person}"
     end
   end
 end
